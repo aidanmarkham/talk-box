6b4a663ce00711dbfa68b25d807975b5a6ff1173
Aidan Markham <apm4337@g.rit.edu>
2021-09-16 10:35:31 -0400

    fix: remove unused property from mission

--- Missions/Mission.cs
+++ Missions/Mission.cs
@@ -8,7 +8,6 @@ namespace TalkBox.Missions
     [CreateAssetMenu(fileName = "New Mission", menuName = "Data/Mission", order = 1)]
     public class Mission : ScriptableObject
     {
-        public Mission MissionTag;
         public string DisplayText;
 
         public string ID = Guid.NewGuid().ToString();
