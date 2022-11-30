d1c369a77552efac06650411b8589fa3ace9ef32
Noah Ratcliff <nhrtclff@gmail.com>
2022-11-30 17:19:27 -0500

    feat: remove clothes from character data

--- Core/CharacterData.cs
+++ Core/CharacterData.cs
@@ -23,13 +23,6 @@ namespace TalkBox.Core
         public int Mouth = 0;
         public int Eyes = 0;
         public int Voice = 0;
-        public int Hat = 0;
-        public int Shirt = 0;
-        public int ShirtMaterial = -1;
-        public int Pants = 0;
-        public int PantsMaterial = -1;
-        public int Shoes = 0;
-        public int ShoesMaterial = -1;
         public int CharacterType;
 
         [Button]
